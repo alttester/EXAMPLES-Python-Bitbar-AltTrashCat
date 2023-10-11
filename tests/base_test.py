02a2d8c501b881dd79c3ae375aff7e95ebea527b
SandyEma9 <sandrina.sere@altom.com>
2023-10-11 15:03:12 +0300

    update to v2.0.2

--- tests/base_test.py
+++ tests/base_test.py
@@ -15,17 +15,27 @@ class TestBase(TestCase):
     @classmethod
     def setUpClass(cls):
 
+        HOST_ALT_SERVER = os.getenv("HOST_ALT_SERVER")
+        BITBAR_APIKEY = os.getenv("BITBAR_APIKEY")
+        BITBAR_APP_ID_SDK_202 = os.getenv("BITBAR_APP_ID_SDK_202")
+
         options = AppiumOptions()
         options.platform_name = 'Android'
         options.automation_name = "UiAutomator2"
+        options.set_capability("bitbar_apiKey", BITBAR_APIKEY)
+        options.set_capability("bitbar_project", "client-side: AltServer on custom host; Android")
+        options.set_capability("bitbar_testrun", "Start Page Tests on Samsung")
         options.set_capability("app", os.path.abspath("application.apk"))
-
+        
+        #See available devices at: https://cloud.bitbar.com/#public/devices
+        options.set_capability("bitbar_device", "Samsung Galaxy A52 -US")
+        options.set_capability("bitbar_app", BITBAR_APP_ID_SDK_202)
         time.sleep(15)
 
         cls.driver = webdriver.Remote(
             'http://localhost:4723/wd/hub', options=options)
         time.sleep(15)
-        cls.altdriver = AltDriver()
+        cls.altdriver = AltDriver(host=HOST_ALT_SERVER)
 
     @classmethod
     def tearDownClass(cls):
